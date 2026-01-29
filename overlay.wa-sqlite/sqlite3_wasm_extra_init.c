fd59d59fa78a52b83b4707f039dda3d4e2e214be
Arslan <81504779+arslanastral@users.noreply.github.com>
2026-01-29 14:34:36 +0800

    sqlite3 multiple cipher test

--- overlay.wa-sqlite/sqlite3_wasm_extra_init.c
+++
@@ -1,9 +0,0 @@
-#include "sqlite3.h"
-
-// This is the entry point for SQLite3 Multiple Ciphers
-extern int sqlite3mc_initialize(void);
-
-int sqlite3_wasm_extra_init(const char *z) {
-  // This registers the PRAGMAs and the VFS shim
-  return sqlite3mc_initialize();
-}
